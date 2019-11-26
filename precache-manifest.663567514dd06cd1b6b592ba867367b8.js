1a4e19ec458f11b08ca90d3be05299b9f81da4d4
Jack <jakexu9946@gmail.com>
2019-11-26 14:22:43 -0500

    Updates

---
+++ precache-manifest.663567514dd06cd1b6b592ba867367b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d48e169ad17fbbe9f3237d226391456",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "573a086edb2c8d56a1d6",
+    "url": "/Pathfinder/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d0fc290c8019477573fd",
+    "url": "/Pathfinder/static/css/main.9b10878c.chunk.css"
+  },
+  {
+    "revision": "573a086edb2c8d56a1d6",
+    "url": "/Pathfinder/static/js/2.06e91f05.chunk.js"
+  },
+  {
+    "revision": "d0fc290c8019477573fd",
+    "url": "/Pathfinder/static/js/main.50112855.chunk.js"
+  },
+  {
+    "revision": "00d5be57cc56e49d5d4e",
+    "url": "/Pathfinder/static/js/runtime-main.b16dfcc4.js"
+  }
+]);
