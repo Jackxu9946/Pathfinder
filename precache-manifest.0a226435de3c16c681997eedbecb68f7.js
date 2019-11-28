c58897767a1912f520edad61258b2bb2e8ca83b2
Jack <jakexu9946@gmail.com>
2019-11-28 14:52:28 -0500

    Updates

--- precache-manifest.0a226435de3c16c681997eedbecb68f7.js
+++ precache-manifest.0a226435de3c16c681997eedbecb68f7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "570e6940b7b53846457428466ec380ec",
+    "revision": "b889a429904804b85604de3a5330d3d1",
     "url": "/Pathfinder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Pathfinder/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "d6a64c1e5abcfd8eebc7",
+    "revision": "dc7cbbfcbb0a2d830e91",
     "url": "/Pathfinder/static/css/main.d37c4a42.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Pathfinder/static/js/2.4578d784.chunk.js"
   },
   {
-    "revision": "d6a64c1e5abcfd8eebc7",
-    "url": "/Pathfinder/static/js/main.4ea3d30a.chunk.js"
+    "revision": "dc7cbbfcbb0a2d830e91",
+    "url": "/Pathfinder/static/js/main.d96743f9.chunk.js"
   },
   {
     "revision": "00d5be57cc56e49d5d4e",
