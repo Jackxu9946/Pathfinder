57785db1d73c1f8c8ad783b5d594ead213b3b9bc
Jack <jakexu9946@gmail.com>
2019-12-08 13:08:23 -0800

    Changed number of columns

--- WebstormProjects/Path/src/PathFindingVisualizer/Path.jsx
+++ WebstormProjects/Path/src/PathFindingVisualizer/Path.jsx
@@ -15,7 +15,7 @@ const START_NODE_COL = 0;
 const FINISH_NODE_ROW = 3;
 const FINISH_NODE_COL = 0;
 const GRID_ROW_LENGTH = 23;
-const GRID_COL_LENGTH = 75;
+const GRID_COL_LENGTH = 70;
 var TIME_OUT_CONST = 25;
 
 export default class Path extends Component {
