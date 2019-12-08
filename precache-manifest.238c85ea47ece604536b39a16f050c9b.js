ed29cf9fbb1866456b292f1d57d5c2b9a67148b7
Jack <jakexu9946@gmail.com>
2019-12-08 12:56:52 -0800

    Updates

--- precache-manifest.238c85ea47ece604536b39a16f050c9b.js
+++ precache-manifest.238c85ea47ece604536b39a16f050c9b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "966df786ddb94ff08421e77af2bc85f3",
+    "revision": "d4a0211bd2a1267c7e4e9c0042196448",
     "url": "/Pathfinder/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Pathfinder/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "558984739ad05115108b",
-    "url": "/Pathfinder/static/css/main.d37c4a42.chunk.css"
+    "revision": "01739cd35de901b758e5",
+    "url": "/Pathfinder/static/css/main.e39c2754.chunk.css"
   },
   {
     "revision": "609b020f90e9d72ae4b7",
     "url": "/Pathfinder/static/js/2.4578d784.chunk.js"
   },
   {
-    "revision": "558984739ad05115108b",
-    "url": "/Pathfinder/static/js/main.35c1ab03.chunk.js"
+    "revision": "01739cd35de901b758e5",
+    "url": "/Pathfinder/static/js/main.4645fdaf.chunk.js"
   },
   {
     "revision": "00d5be57cc56e49d5d4e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pathfinder/precache-manifest.22844f84372f859f5310094112ec2540.js"
+  "/Pathfinder/precache-manifest.238c85ea47ece604536b39a16f050c9b.js"
 );
 
 self.addEventListener('message', (event) => {
